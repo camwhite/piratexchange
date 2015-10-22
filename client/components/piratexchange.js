d448571ce54f53f2524d59c54141524691573334
Cameron White <camw85@mac.com>
2015-10-22 00:42:41 -0400

    :heavy_plus_sign: base for matchmaking with maps api

--- client/components/piratexchange.js
+++ client/components/piratexchange.js
@@ -16,7 +16,20 @@ import {Upload} from 'components/upload';
 export class Piratexchange {
   constructor(locator: Location, socket: Socket) {
     locator.getLocation()
-    .then((coords) => this.location = coords)
+    .then((coords) => {
+      this.location = {
+        lat: coords.latitude,
+        lng: coords.longitude
+      }
+    })
     .catch((err) => console.log(err));
+  }
+  match() {
+    let geo = google.maps.geometry.spherical;
+    let p1 = new google.maps.LatLng(this.location.lat, this.location.lng);
+    let p2 = new google.maps.LatLng(50.087692, 14.421150);
+
+    this.distance = geo.computeDistanceBetween(p1, p2);
+    console.log(this.distance);
   }
 }
