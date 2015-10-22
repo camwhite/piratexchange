e374727ea02fcf7f609c552a73301ce564b00473
Cameron White <camw85@mac.com>
2015-10-21 23:13:12 -0400

    :heavy_plus_sign: clientside router

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import {ComponentMetadata as Component, ViewMetadata as View, bootstrap} from 'angular2/angular2';
+import {Router, RouterOutlet, RootRouter} from 'angular2/router';
 import {Location} from 'services/location';
 import {WebRTC} from 'services/webrtc';
 import {Socket} from 'services/socket';
@@ -10,14 +11,23 @@ import {Piratexchange} from 'components/piratexchange';
 })
 
 @View({
-  directives: [Piratexchange],
+  directives: [RouterOutlet],
   template: `
-    <piratexchange></piratexchange>
+    <router-outlet></router-outlet>
   `
 })
 
 class Main {
-
+  constructor(router: Router) {
+    this.router = router;
+    // Here we configure, for each route, which component should be added and its alias for URL linking
+    router
+      .config('/', Piratexchange, 'piratexchange')
+      .then((_) => router.navigate('/'))
+  }
 }
 
-bootstrap(Main);
+bootstrap(Main, [
+  // Here we're creating the Router.
+  // We're also configuring DI, so that each time a Router is requested, it's automatically returned.
+  bind(Router).toValue(new RootRouter(new Pipeline())));
